f26fe2142f4ead3bfdee5fc38f26710d80c34fb1
jomlambo <lambertjefferson66@gmail.com>
2022-02-12 18:47:45 +0000

    fix gpa input

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-function findGrade(){
-let a = document.getElementById("201Grade")
-document.getElementById("foo").innerHTML=a.value
+//irrelevant function:
+// function findGrade(){
+// let a = document.getElementById("201Grade")
+// document.getElementById("foo").innerHTML=a.value
+// }
+
+//function to pull the value of all class grades (checkboxes) and put it into a decimal value
+function takeGrade(gradeOne, gradeTwo, gradeThree, gradeFour, gradeFive) {
+
 }
 
-// function 
+//function to take the various decimal values and put the weightings into the calculation
+function weightGrade(gradeOne, gradeTwo, gradeThree, gradeFour, gradeFive) {
 
+}
 
-
+//final function to pull all data and create the output
 function calculateTotal(){
 
 }
